05fa8bab38ff2b2d7bd159e28d90de4a3d0ef881
Sam55wats <39461108+Sam55wats@users.noreply.github.com>
2026-06-12 10:49:16 -0400

    created Restaurant viewset

--- apps/restaurants/views.py
+++ apps/restaurants/views.py
@@ -1,7 +1,14 @@
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Restaurant
+from .serializers import RestaurantSerializer
 
 @api_view(["GET"])
 def health_check(request):
     return Response({"status": "ok"})
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
