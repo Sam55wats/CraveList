4c7225a1b47ae734b9371700b20f5fb4d5457ee9
Sam55wats <39461108+Sam55wats@users.noreply.github.com>
2026-06-14 12:36:41 -0400

    added meta class to order restaurants in descending order

--- apps/restaurants/models.py
+++ apps/restaurants/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     cuisine = models.CharField(max_length=255, blank=True)
@@ -12,6 +13,9 @@ class Restaurant(models.Model):
     visited_notes = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
 
     def __str__(self):
         return self.name
