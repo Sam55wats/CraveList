237b823e07347a1bcd972eacfd623df073af0c34
Sam55wats <39461108+Sam55wats@users.noreply.github.com>
2026-06-15 15:24:50 -0400

    updated urls

--- apps/restaurants/urls.py
+++ apps/restaurants/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import RestaurantViewSet
+from .views import FollowViewSet, RestaurantViewSet, UserRestaurantViewSet
 
 router = DefaultRouter()
 router.register("restaurants", RestaurantViewSet, basename="restaurant")
+router.register("my-restaurants", UserRestaurantViewSet, basename="my-restaurant")
+router.register("follows", FollowViewSet, basename="follow")
 
 urlpatterns = [
     path("", include(router.urls)),
