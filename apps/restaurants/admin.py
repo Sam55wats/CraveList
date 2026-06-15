4adebd3daad40c40fbefbdba478a82fe86af3d35
Sam55wats <39461108+Sam55wats@users.noreply.github.com>
2026-06-15 15:25:28 -0400

    registered user restaurant and follow in admin

--- apps/restaurants/admin.py
+++ apps/restaurants/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Restaurant
+
+from .models import Follow, Restaurant, UserRestaurant
 
 
 @admin.register(Restaurant)
@@ -8,21 +9,63 @@ class RestaurantAdmin(admin.ModelAdmin):
         "name",
         "cuisine",
         "city",
+        "state",
         "price_level",
-        "visited",
-        "rating",
+        "external_source",
         "updated_at",
     )
     list_filter = (
-        "visited",
         "price_level",
         "cuisine",
         "city",
+        "state",
+        "external_source",
     )
     search_fields = (
         "name",
+        "address",
         "cuisine",
         "city",
-        "vibe",
+        "state",
+        "external_place_id",
+    )
+
+
+@admin.register(UserRestaurant)
+class UserRestaurantAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "restaurant",
+        "bookmarked",
+        "visited",
+        "rating",
+        "visited_at",
+        "updated_at",
+    )
+    list_filter = (
+        "bookmarked",
+        "visited",
+        "rating",
+        "restaurant__cuisine",
+        "restaurant__city",
+    )
+    search_fields = (
+        "user__username",
+        "restaurant__name",
+        "restaurant__city",
         "notes",
+    )
+    autocomplete_fields = ("restaurant",)
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = (
+        "follower",
+        "following",
+        "created_at",
+    )
+    search_fields = (
+        "follower__username",
+        "following__username",
     )
