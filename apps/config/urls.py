bfef0132460da631c5bea5472d3c8c21e43eab42
Sam55wats <39461108+Sam55wats@users.noreply.github.com>
2026-07-08 16:16:03 -0400

    added account aunder api/auth

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
 from restaurants.views import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/health/', health_check, name='health-check'),
+    path("api/auth/", include("accounts.urls")),
     path("api/", include("restaurants.urls")),
-
 ]
