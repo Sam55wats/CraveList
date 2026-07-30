3dae41e05460aa515522c5c3df05fc463eee7846
Sam55wats <39461108+Sam55wats@users.noreply.github.com>
2026-07-30 15:40:44 -0400

    added csrf token for session auth

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,18 @@
 from django.contrib.auth import authenticate, login, logout
+from django.middleware.csrf import get_token
+from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework import permissions, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from .serializers import RegisterSerializer, UserSerializer
+
+
+@api_view(["GET"])
+@permission_classes([permissions.AllowAny])
+@ensure_csrf_cookie
+def csrf(request):
+    return Response({"csrfToken": get_token(request)})
 
 
 @api_view(["POST"])
