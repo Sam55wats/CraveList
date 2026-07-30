d4bca302c823181180c2aa910a57a62bfc02fb0f
Sam55wats <39461108+Sam55wats@users.noreply.github.com>
2026-07-30 15:41:05 -0400

    added url for csrf

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    path("csrf/", views.csrf, name="csrf"),
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
