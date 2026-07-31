67dc3ed593d15c3865156fe2fbd18c3426fb2274
Sam55wats <39461108+Sam55wats@users.noreply.github.com>
2026-07-31 13:17:48 -0400

    created csrf enpoint for seesion based requests

--- apps/config/settings.py
+++ apps/config/settings.py
@@ -27,6 +27,23 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+CSRF_COOKIE_SAMESITE = "Lax"
+SESSION_COOKIE_SAMESITE = "Lax"
+
 
 # Application definition
 
--- apps/config/urls.py
+++ apps/config/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from restaurants.views import health_check
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path("api/auth/", include("accounts.urls")),
     path("api/", include("restaurants.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
